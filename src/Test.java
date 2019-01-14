0414893b407cb9d99d01c485bb53146f9e9aaae0
Jonnel21 <ajonnel@gmail.com>
2019-01-14 12:42:43 -0800

    added borderlayout to panel & .pack method onto frame

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,6 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
@@ -14,80 +17,79 @@ import org.jsoup.select.*;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-		
+		/*Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = screenSize.width;
+		int height = screenSize.height;*/
+
 		File input = new File("test.html");
 		try {
 			Document doc = Jsoup.parse(input, "UTF-8", "");
-			//System.out.println(doc);
-			
+			// System.out.println(doc);
+
 			Elements title = doc.getElementsByTag("h1");
-			//System.out.println(title.text());
-			
+			// System.out.println(title.text());
+
 			Elements res = doc.getElementsByClass("result");
-			//System.out.println(res.text());
-			
-			Frame frame = new Frame("test");
+			// System.out.println(res.text());
+
+			// MainFrame frame = new MainFrame("test", width, height);
+			MainFrame frame = new MainFrame("test");
 			JPanel panel = new JPanel();
 			JButton browse = new JButton("Browse");
 			JButton send = new JButton("send");
 			JTextField text = new JTextField(30);
 			final JFileChooser fc = new JFileChooser();
-			
+
 			text.setEnabled(false);
-			
-			send.setSize(50, 50);
+
 			send.addActionListener(new ActionListener() {
 
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					// TODO: get data from file and send to google sheets
-					/*System.out.println(title.text());
-					System.out.println(res.text());*/
-					if(text.getText().isEmpty()) {
+					/*
+					 * System.out.println(title.text()); System.out.println(res.text());
+					 */
+					if (text.getText().isEmpty()) {
 						System.out.println("Error: Please select a compatible file");
-					}
-					else {
+					} else {
 						System.out.println("Data was successfully sent to Google Sheets!");
 					}
 				}
-				
+
 			});
-			
-			browse.setSize(100, 100);
+
 			browse.addActionListener(new ActionListener() {
 
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
 					int returnVal = fc.showOpenDialog(browse);
-					
-					if(returnVal == JFileChooser.APPROVE_OPTION) {
+
+					if (returnVal == JFileChooser.APPROVE_OPTION) {
 						File f = fc.getSelectedFile();
 						System.out.println(f.getName());
 						text.setText(f.getName());
 					}
-					
+
 				}
-				
+
 			});
-			
-			
-			panel.setSize(500, 500);
-			
+
+			frame.setLayout(new BorderLayout());
+
 			panel.add(text);
 			panel.add(browse);
 			panel.add(send);
-		
-			frame.add(panel);
-			frame.setSize(panel.getWidth(), panel.getHeight());
+
+			frame.add(panel, BorderLayout.CENTER);
+			frame.pack();
 			frame.setVisible(true);
-			
+
 		} catch (IOException e) {
 			System.out.println("Error!");
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 }
